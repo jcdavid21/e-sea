c4da222df97927fe5917d1ab546988e3923150c2
jcdavid15 <juancarlodavid15@gmail.com>
2025-12-06 12:58:25 +0800

    updated

--- seller-service/index.js
+++ seller-service/index.js
@@ -2529,7 +2529,7 @@ app.get("/api/buyer/debug/buyers", async (req, res) => {
 // =============================
 //  Server Initialization
 // =============================
-const PORT = 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`
