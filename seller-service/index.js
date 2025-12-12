91cd1a1029b16934103f51fe9b88db02fa3ea241
jcdavid15 <juancarlodavid15@gmail.com>
2025-12-12 17:14:24 +0800

    updated

--- seller-service/index.js
+++ seller-service/index.js
@@ -62,7 +62,7 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DATABASE_NAME,
-  port: process.env.DB_PORT,
+  // port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
